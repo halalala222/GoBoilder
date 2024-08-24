4da281721fb32f1b58f90173c1cf6b514d51063b
halalala222 <1741196223@qq.com>
2024-08-24 23:13:31 +0800

    feat: builder add generateAllBuilder

--- internal/build/builder.go
+++ internal/build/builder.go
@@ -3,3 +3,11 @@ package build
 type Builder interface {
 	Build() error
 }
+
+func GenerateAllBuilder(options ...Option) []Builder {
+	opts := getOptions(options)
+
+	return []Builder{
+		NewProjectBuilder(opts.projectName),
+	}
+}
