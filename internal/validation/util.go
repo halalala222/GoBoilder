a5e3a31a4be39de91eef0fa0a89cbe4cb523afa8
halalala222 <1741196223@qq.com>
2024-08-24 16:40:06 +0800

    feat: add validation util

---
+++ internal/validation/util.go
@@ -0,0 +1,27 @@
+package validation
+
+import (
+	"os"
+
+	"github.com/halalala222/GoBoilder/internal/constants"
+)
+
+func CheckProjectName(projectName string) error {
+	var (
+		err error
+	)
+
+	if projectName == "" {
+		return constants.ErrProjectNameEmpty
+	}
+
+	if _, err = os.Stat(projectName); os.IsExist(err) {
+		return constants.ErrProjectNameExists
+	}
+
+	if os.IsNotExist(err) {
+		return nil
+	}
+
+	return err
+}
