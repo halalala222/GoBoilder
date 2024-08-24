74fc93cc37533c57ae951e8fb9758dc7f9bef917
halalala222 <1741196223@qq.com>
2024-08-24 16:40:15 +0800

    feat: add define errors

---
+++ internal/constants/errors.go
@@ -0,0 +1,12 @@
+package constants
+
+import "errors"
+
+var (
+	ErrLoggerLibraryNotSupported = errors.New("logger library not supported")
+	ErrHTTPFrameNotSupported     = errors.New("http frame not supported")
+	ErrQuit                      = errors.New("quit")
+
+	ErrProjectNameEmpty  = errors.New("project name cannot be empty")
+	ErrProjectNameExists = errors.New("project name already exists")
+)
