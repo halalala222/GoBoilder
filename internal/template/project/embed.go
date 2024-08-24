8b4f23a0a65dbf75c4cfc77aa114e2887f6a6186
halalala222 <1741196223@qq.com>
2024-08-24 23:13:01 +0800

    feat: update project embed

--- internal/template/project/embed.go
+++ internal/template/project/embed.go
@@ -4,3 +4,9 @@ import _ "embed"
 
 //go:embed gitignore.tmpl
 var GitIgnoreTemplate []byte
+
+//go:embed README.tmpl
+var ReadmeTemplate []byte
+
+//go:embed Makefile.tmpl
+var MakefileTemplate []byte
