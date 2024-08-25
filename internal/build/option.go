8c52a1d50e7a9ac9e253739ffe7497fb1df7ee3d
halalala222 <1741196223@qq.com>
2024-08-25 16:12:28 +0800

    feat: option add logger library & module path

--- internal/build/option.go
+++ internal/build/option.go
@@ -1,7 +1,9 @@
 package build
 
 type buildOptions struct {
-	projectName string
+	projectName   string
+	loggerLibrary string
+	modulePath    string
 }
 
 type Option func(*buildOptions)
@@ -15,6 +17,18 @@ func getDefaultBuildOption() *buildOptions {
 func WithProjectName(projectName string) Option {
 	return func(o *buildOptions) {
 		o.projectName = projectName
+	}
+}
+
+func WithLoggerLibrary(loggerLibrary string) Option {
+	return func(o *buildOptions) {
+		o.loggerLibrary = loggerLibrary
+	}
+}
+
+func WithModulePath(modulePath string) Option {
+	return func(o *buildOptions) {
+		o.modulePath = modulePath
 	}
 }
 
