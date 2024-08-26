c990a887c17ae52712ba1429671611c1a55f263b
halalala222 <1741196223@qq.com>
2024-08-26 20:54:25 +0800

    feat: change articleTemplate to userTemplate

--- internal/template/domain/embed.go
+++ internal/template/domain/embed.go
@@ -2,8 +2,8 @@ package domain
 
 import _ "embed"
 
-//go:embed article.go.tmpl
-var ArticleTemplate []byte
+//go:embed user.go.tmpl
+var UserTemplate []byte
 
 //go:embed errors.go.tmpl
 var ErrorsTemplate []byte
