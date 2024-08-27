a7053a8cd2bd35f15ff26b953c5f5e33c5c13b10
halalala222 <1741196223@qq.com>
2024-08-27 20:12:06 +0800

    feat: update errors info

--- internal/constants/errors.go
+++ internal/constants/errors.go
@@ -12,8 +12,8 @@ var (
 	ErrProjectNameStartOrEndWithDot                     = errors.New("project name cannot start or end with dot")
 	ErrProjectModulePathPrefixStartOrEndWithSlash       = errors.New("module path prefix cannot start or end with slash")
 	ErrProjectModulePathPrefixContainsDoubleSlash       = errors.New("module path prefix cannot contain double slash")
-	ErrProjectModulePathContainsWindowsReservedFileName = errors.New("module path contains windows reserved file name")
-	ErrProjectModulePathEndWithTildeFollowedByDigits    = errors.New("module path end with tilde followed by digits")
+	ErrProjectModulePathContainsWindowsReservedFileName = errors.New("module path contains windows reserved file name before the first dot")
+	ErrProjectModulePathEndWithTildeFollowedByDigits    = errors.New("module path end with tilde followed by digits before the first dot")
 	ErrInvalidASCIICharacters                           = errors.New("invalid ASCII characters")
 
 	ErrModulePathEmpty = errors.New("module path cannot be empty")
