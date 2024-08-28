4234cba03ec091611f99f4e58f551b8bba8e7d7e
halalala222 <1741196223@qq.com>
2024-08-28 20:59:07 +0800

    feat: add db tmpl file

--- internal/template/config/embed.go
+++ internal/template/config/embed.go
@@ -4,3 +4,20 @@ import _ "embed"
 
 //go:embed gorm.go.tmpl
 var GormConfigTemplate []byte
+
+//go:embed database_sql_mysql.go.tmpl
+var DatabaseSQLMySQLConfigTemplate []byte
+
+//go:embed database_sql_postgresql.go.tmpl
+var DatabaseSQLPostgreSQLConfigTemplate []byte
+
+//go:embed database_sql_sqlite.go.tmpl
+var DatabaseSQLSQLiteConfigTemplate []byte
+
+//go:embed mongo_driver_mongodb.go.tmpl
+var MongoDriverMongoDBConfigTemplate []byte
+
+//go:embed database_sql_sqlite.go.tmpl
+
+//go:embed gin.go.tmpl
+var GinConfigTemplate []byte
