e079a4d9946652e9caeb9d06c3451a0cb4a47d24
halalala222 <1741196223@qq.com>
2024-08-29 11:46:01 +0800

    feat: change config db hold supportedDB & add GetDBLibraryInfo

--- internal/config/db.go
+++ internal/config/db.go
@@ -1,10 +1,60 @@
 package config
 
-var supportedDB = map[string][]string{
-	"MySQL":      {"GORM", "go-sql-driver/mysql"},
-	"PostgreSQL": {"GORM", "jackc/pgx"},
-	"SQLite":     {"GORM", "mattn/go-sqlite3"},
-	"MongoDB":    {"mongo-go-driver/mongo"},
+import (
+	"github.com/halalala222/GoBoilder/internal/constants"
+	"github.com/halalala222/GoBoilder/internal/template/config"
+)
+
+type DBLibraryInfo struct {
+	name     string
+	FileName string
+	Template []byte
+}
+
+var supportedDB = map[string][]*DBLibraryInfo{
+	constants.DataBaseMySQL: {
+		{
+			name:     constants.DatabaseLibraryGorm,
+			FileName: constants.GormConfigFileName,
+			Template: config.GormConfigTemplate,
+		},
+		{
+			name:     constants.DatabaseLibraryDatabaseSQL,
+			FileName: constants.DatabaseSQLMySQLConfigFileName,
+			Template: config.DatabaseSQLMySQLConfigTemplate,
+		},
+	},
+	constants.DataBasePostgreSQL: {
+		{
+			name:     constants.DatabaseLibraryGorm,
+			FileName: constants.GormConfigFileName,
+			Template: config.GormConfigTemplate,
+		},
+		{
+			name:     constants.DatabaseLibraryDatabaseSQL,
+			FileName: constants.DatabaseSQLPostgreSQLConfigFileName,
+			Template: config.DatabaseSQLPostgreSQLConfigTemplate,
+		},
+	},
+	constants.DataBaseSQLite: {
+		{
+			name:     constants.DatabaseLibraryGorm,
+			FileName: constants.GormConfigFileName,
+			Template: config.GormConfigTemplate,
+		},
+		{
+			name:     constants.DatabaseLibraryDatabaseSQL,
+			FileName: constants.DatabaseSQLSQLiteConfigFileName,
+			Template: config.DatabaseSQLSQLiteConfigTemplate,
+		},
+	},
+	constants.DataBaseMongoDB: {
+		{
+			name:     constants.DatabaseLibraryMongoDriver,
+			FileName: constants.MongoDriverMongoDBConfigFileName,
+			Template: config.MongoDriverMongoDBConfigTemplate,
+		},
+	},
 }
 
 // GetSupportedDB returns the supported databases.
@@ -21,13 +71,38 @@ func GetSupportedDB() []string {
 // GetDBLibraries returns the libraries for the given database.
 func GetDBLibraries(db string) []string {
 	var (
-		libraries []string
-		ok        bool
+		libraries   []string
+		ok          bool
+		dbLibraries []*DBLibraryInfo
 	)
 
-	if libraries, ok = supportedDB[db]; !ok {
+	if dbLibraries, ok = supportedDB[db]; !ok {
 		return make([]string, 0)
 	}
 
+	for _, library := range dbLibraries {
+		libraries = append(libraries, library.name)
+	}
+
 	return libraries
+}
+
+// GetDBLibraryInfo returns the library info for the given database and library.
+func GetDBLibraryInfo(db, library string) (*DBLibraryInfo, error) {
+	var (
+		dbLibraries []*DBLibraryInfo
+		ok          bool
+	)
+
+	if dbLibraries, ok = supportedDB[db]; !ok {
+		return nil, constants.ErrDBNotSupported
+	}
+
+	for _, lib := range dbLibraries {
+		if lib.name == library {
+			return lib, nil
+		}
+	}
+
+	return nil, constants.ErrDBLibraryNotSupported
 }
