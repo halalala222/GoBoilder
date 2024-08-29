ec363eabb59d1a8f8aa9ed57f49b2831e687d521
halalala222 <1741196223@qq.com>
2024-08-29 20:43:48 +0800

    feat: add embed.go

--- internal/template/repository/embed.go
+++ internal/template/repository/embed.go
@@ -4,3 +4,15 @@ import _ "embed"
 
 //go:embed gorm.go.tmpl
 var GormTemplate []byte
+
+//go:embed mongodb.go.tmpl
+var MongodbTemplate []byte
+
+//go:embed mysql.go.tmpl
+var MysqlTemplate []byte
+
+//go:embed postgresql.go.tmpl
+var PostgresqlTemplate []byte
+
+//go:embed sqlite.go.tmpl
+var SqliteTemplate []byte
