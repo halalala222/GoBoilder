c3caec4a9debcab2fb5dc1bc800f7768eee15d36
halalala222 <1741196223@qq.com>
2024-08-29 20:41:45 +0800

    feat: add db repository to get repository template

---
+++ internal/repository/repository.go
@@ -0,0 +1,34 @@
+package repository
+
+import "github.com/halalala222/GoBoilder/internal/constants"
+
+type Template interface {
+	Build() []byte
+}
+
+func db2Template() map[string]Template {
+	return map[string]Template{
+		constants.DataBaseMySQL + constants.DatabaseLibraryGorm:             &GormRepositoryTemplate{},
+		constants.DataBaseMySQL + constants.DatabaseLibraryDatabaseSQL:      &MySQLRepositoryTemplate{},
+		constants.DataBasePostgreSQL + constants.DatabaseLibraryGorm:        &GormRepositoryTemplate{},
+		constants.DataBasePostgreSQL + constants.DatabaseLibraryDatabaseSQL: &PostgreSQLRepositoryTemplate{},
+		constants.DataBaseSQLite + constants.DatabaseLibraryGorm:            &GormRepositoryTemplate{},
+		constants.DataBaseSQLite + constants.DatabaseLibraryDatabaseSQL:     &SQLiteRepositoryTemplate{},
+		constants.DataBaseMongoDB:                                           &MongoDBRepositoryTemplate{},
+	}
+}
+
+// GetRepositoryTemplate returns the repository template based on the database
+func GetRepositoryTemplate(database string, dbLibrary string) (Template, error) {
+	var (
+		dbTemplate = db2Template()
+		template   Template
+		ok         bool
+	)
+
+	if template, ok = dbTemplate[database+dbLibrary]; !ok {
+		return nil, constants.ErrDBOrDBLibraryNotSupported
+	}
+
+	return template, nil
+}
