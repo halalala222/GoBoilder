42c3bdcf8fa233482f256ef8a1b81da4816785b6
halalala222 <1741196223@qq.com>
2024-08-30 23:33:30 +0800

    perf: change name to improve readability

--- internal/template/logger/embed.go
+++ internal/template/logger/embed.go
@@ -3,10 +3,10 @@ package logger
 import _ "embed"
 
 //go:embed pkg_logger.go.tmpl
-var Logger []byte
+var PkgLogger []byte
 
 //go:embed internal_log.go.tmpl
-var Log []byte
+var InternalLog []byte
 
 //go:embed zap_logger.go.tmpl
 var ZapLoggerTemplate []byte
